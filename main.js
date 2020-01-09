1a07754f1d0625074cbef4b00373d001c4dc5b23
Manraj <manraj836@gmail.com>
2020-01-09 03:24:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -142,21 +142,6 @@ var urls = [
 
 var target = "epilepsy";
 
-$(document).ready(function() {
-    $(window).scroll( function(){
-        $('.glitch-home').each( function(i){
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},500);
-            }
-            
-        }); 
-    
-    });
-    
-});
-
 //hides nav until scroll down
 $(window).on("load", function(){
 	mainNav();
